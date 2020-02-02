cb96b9f8a6e99827db71464bc6643cc79d4eb5a5
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-02-02 13:14:01 -0600

    refactor: remove typescript boilerplate

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import Link from 'next/link'
 import Head from 'next/head'
 
 type Props = {
@@ -16,26 +15,7 @@ const Layout: React.FunctionComponent<Props> = ({
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>
-      </nav>
-    </header>
     {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
   </div>
 )
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import Link from 'next/link'
 import Layout from '../components/Layout'
 import { NextPage } from 'next'
 
@@ -7,11 +6,6 @@ const IndexPage: NextPage = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>Hello Next.js 👋</h1>
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
     </Layout>
   )
 }
